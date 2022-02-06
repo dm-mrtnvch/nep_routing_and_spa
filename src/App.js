7bfc150c13eea8c7d99130cb9baef407b1fd5f62
dm-mrtnvch <dmvshn@gmail.com>
2022-02-07 01:51:11 +0300

    83

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
+import {BrowserRouter as Router, Route ,Switch} from 'react-router-dom';
+import {Home} from './components/pages/Home';
+import {Contact} from './components/pages/Contact';
+import {About} from './components/pages/About';
+import {NotFound} from './components/pages/NotFound';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header/>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route  path="/contact" component={Contact}/>
+          <Route  path="/about" component={About}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </Router>
+      <main className="container content">
 
-    </div>
+
+      </main>
+      <Footer/>
+    </>
   );
 }
 
